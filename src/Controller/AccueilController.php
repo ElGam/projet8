87af9d53d9ffa11a4204ea0b74613eaa0bf7c901
Mederick77 <mederick.delos@gmail.com>
2021-09-23 17:20:24 +0200

    Add Templates + Authentication

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccueilController extends AbstractController
+{
+    /**
+     * @Route("/", name="accueil")
+     */
+    public function index(): Response
+    {
+        $user = $this->getUser();       
+
+        return $this->render('accueil/index.html.twig', [
+            'controller_name' => 'AccueilController',
+        ]);
+    }
+}
